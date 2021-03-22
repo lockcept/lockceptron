d69c7158c5944cfc99b66e57942db93399e10b41
lockcept <lockcept@gmail.com>
2021-03-22 23:19:25 +0900

    chore: embed what's new

--- src/messages/helpMessageListener.ts
+++ src/messages/helpMessageListener.ts
@@ -1,10 +1,11 @@
+import { MessageEmbed } from 'discord.js';
 import { helpDoc } from '../config';
 import { MessageListener } from '../helpers/addMessageListener';
 
 const help:MessageListener = (msg, message) => {
   if (message === 'help') {
     msg.channel.send('Hi! I am lockceptron');
-    msg.channel.send(helpDoc);
+    msg.channel.send(new MessageEmbed({ title: 'What\'s new?', url: helpDoc }));
   }
 };
 
