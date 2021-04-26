5f4328b675f9825c2fd799e341a95e23ab734966
lockcept <lockcept@gmail.com>
2021-04-27 03:01:28 +0900

    random message listener

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import Discord from "discord.js";
 import addMessageListener from "./helpers/addMessageListener";
 import help from "./messages/helpMessageListener";
 import memo from "./messages/memoMessageListener";
+import randomPick from "./messages/randomMessageListener";
 
 const main = (client: Discord.Client) => {
   addMessageListener(client, help);
   addMessageListener(client, memo);
+  addMessageListener(client, randomPick);
 };
 
 export default main;
