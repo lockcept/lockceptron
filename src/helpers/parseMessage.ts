930b5e58248344f5008301cabe9a83f818860884
lockcept <lockcept@gmail.com>
2021-05-23 18:26:51 +0900

    fix: parse error

--- src/helpers/parseMessage.ts
+++ src/helpers/parseMessage.ts
@@ -5,7 +5,7 @@ const parseMessage = (msg: string): string | null => {
   if (stage === "prod") command = "tron";
   else if (stage === "dev") command = "tron-dev";
 
-  if (msg.startsWith(command)) {
+  if (msg.startsWith(`${command} `)) {
     return msg.substring(command.length + 1);
   }
   return null;
