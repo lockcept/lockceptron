0a3c68e0bfc74c8fe39a4b47f82b82fe75665644
lockcept <lockcept@gmail.com>
2021-07-31 21:24:02 +0900

    chore: simplify main

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import Discord from "discord.js";
+import { map } from "lodash";
 import addMessageListener from "./helpers/addMessageListener";
 import help from "./messages/helpMessageListener";
 import memo from "./messages/memoMessageListener";
 import randomPick from "./messages/randomMessageListener";
 
 const main = (client: Discord.Client) => {
-  addMessageListener(client, help);
-  addMessageListener(client, memo);
-  addMessageListener(client, randomPick);
+  const messageListeners = [help, memo, randomPick];
+  map(messageListeners, (messageListener) =>
+    addMessageListener(client, messageListener)
+  );
 };
 
 export default main;
