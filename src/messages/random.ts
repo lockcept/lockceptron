b5ef9de8651bc1b778fc4ab7042323f20e55e859
lockcept <lockcept@gmail.com>
2021-08-19 22:27:15 +0900

    refactor: randomService

--- src/messages/random.ts
+++ src/messages/random.ts
@@ -1,51 +1,34 @@
-import { chain, random, range } from "lodash";
 import yargsParser from "yargs-parser";
+import { pickRandomMulti, pickRandomOnce } from "../services/randomService";
 import { MessageListener } from "../helpers/addMessageListener";
 import logger from "../helpers/logger";
+import substring from "../helpers/substring";
 
-const randomPick: MessageListener = (msg, message) => {
+const randomPick: MessageListener = async (msg, message) => {
   if (message.startsWith("random ")) {
-    const MAX_TRY = 1000000;
-    const args = yargsParser(message.substring(7));
-    const number: number = args.n ? parseInt(args.n, 10) : 0;
-    if (!number && args.n) {
-      msg.channel.send("-n argument invalid");
+    const args = yargsParser(substring(message, "random"));
+    const count: number = args.n ? parseInt(args.n, 10) : 0;
+    if (args.n && !count) {
+      msg.channel.send("invalid argument {n}");
       logger.log("[random]: invalid argument", { args: args.n });
-    }
-    const candidates = args._;
-    if (candidates.length === 0) return;
-    logger.log("random args", args);
-    const getOne = () => {
-      return candidates[random(candidates.length - 1)];
-    };
-    const getN = (n: number) => {
-      return chain(range(n))
-        .map(() => {
-          return random(candidates.length - 1);
-        })
-        .countBy()
-        .map((cnt, idx) => {
-          return {
-            cnt,
-            idx,
-          };
-        })
-        .sortBy("cnt")
-        .reverse()
-        .map(({ cnt, idx }) => {
-          return `${candidates[Number(idx)]}: ${cnt}`;
-        })
-        .join("\n")
-        .value();
-    };
-    if (!number || number < 0) {
-      msg.channel.send(getOne());
       return;
     }
 
-    const n = number > MAX_TRY ? MAX_TRY : number;
-    if (number > MAX_TRY) msg.channel.send(`max: ${MAX_TRY}`);
-    msg.channel.send(getN(n));
+    const candidates = args._;
+    if (candidates.length === 0) {
+      msg.channel.send("invalid candidates");
+      logger.log("[random]: invalid candidates");
+      return;
+    }
+
+    logger.log("random args", args);
+
+    if (!count || count < 0) {
+      await pickRandomOnce(msg.channel, candidates);
+      return;
+    }
+
+    await pickRandomMulti(msg.channel, candidates, count);
   }
 };
 
