3ff7cddf3948da35e605e5ab63b57ab7b0d8673c
lockcept <lockcept@gmail.com>
2021-08-19 21:52:26 +0900

    refactor: debugService

--- src/messages/debug.ts
+++ src/messages/debug.ts
@@ -1,17 +1,12 @@
-import { MessageEmbed } from "discord.js";
 import { stage } from "../config";
 
 import { MessageListener } from "../helpers/addMessageListener";
+import { sendDebug } from "../services/debugService";
 
 const debug: MessageListener = async (msg, message) => {
   if (stage !== "dev") return;
   if (message === "debug") {
-    await msg.channel.send(
-      new MessageEmbed({
-        title: "LOCKCEPTRON",
-        description: "test <@!253932693207646209>",
-      })
-    );
+    sendDebug(msg);
   }
 };
 
