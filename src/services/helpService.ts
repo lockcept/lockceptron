e589cf52d412db3e7105130d7aa31a35e94e5430
lockcept <lockcept@gmail.com>
2021-09-21 00:48:03 +0900

    fix: await helpService

--- src/services/helpService.ts
+++ src/services/helpService.ts
@@ -4,8 +4,8 @@ import DiscordChannel from "../helpers/discordChannel";
 
 // eslint-disable-next-line import/prefer-default-export
 export const sendHelp = async (channel: DiscordChannel) => {
-  channel.send("Hi! I am lockceptron");
-  channel.send({
+  await channel.send("Hi! I am lockceptron");
+  await channel.send({
     embeds: [
       new MessageEmbed({
         title: "LOCKCEPTRON",
