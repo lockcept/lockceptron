f3844a79f42f76d4f4f146ebb7559ecd8e97cd5b
lockcept <lockcept@gmail.com>
2021-09-23 02:03:25 +0900

    refactor: move getUsersFromIds

--- src/helpers/parseDiscordId.ts
+++ src/helpers/parseDiscordId.ts
@@ -1,3 +1,7 @@
+import Discord from "discord.js";
+import { chain, map } from "lodash";
+import { fetchMembers, getUsersFromRole } from "./getUsersFromRole";
+
 export const getUserId = (cmd: string): string | null => {
   const result = cmd.match(/^<@!([0-9]+)>$/i);
   if (!result) return null;
@@ -8,4 +12,28 @@ export const getRoleId = (cmd: string): string | null => {
   const result = cmd.match(/^<@&([0-9]+)>$/i);
   if (!result) return null;
   return result[1];
+};
+
+export const getUsersFromIds = async (
+  guild: Discord.Guild,
+  userOrRoles: string[]
+): Promise<string[]> => {
+  await fetchMembers(guild);
+
+  const userIdSet = await Promise.all(
+    userOrRoles.map(async (userOrRole) => {
+      if (getRoleId(userOrRole)) {
+        const role = getRoleId(userOrRole);
+        if (!role) return [];
+        const users = await getUsersFromRole(guild, role);
+        return map(users, (user) => user.id);
+      }
+
+      const userId = getUserId(userOrRole);
+      return userId ? [userId] : [];
+    })
+  );
+
+  const userIds = chain(userIdSet).flatten().compact().uniq().value();
+  return userIds;
 };
